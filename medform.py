21d93337ecd31884913bd881d26eb9c7ed22bea3
Dzan Operta <dzanoperta@yahoo.com>
2020-06-16 02:44:53 +0200

    conf host for production

--- medform.py
+++ medform.py
@@ -26,4 +26,4 @@ def result():
       return render_template("result.html",result = result, message=response.content)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host='0.0.0.0', debug = True)
